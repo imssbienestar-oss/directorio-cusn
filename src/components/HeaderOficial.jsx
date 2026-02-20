a42615ddd1dae193d818380655f8985a995f4979
imssbienestar-oss <irving.hernandezimssb@gmail.com>
2026-02-20 16:00:16 -0600

    Update HeaderOficial.jsx

--- src/components/HeaderOficial.jsx
+++ src/components/HeaderOficial.jsx
@@ -47,9 +47,9 @@ const HeaderOficial = () => {
               <NavLink to="/informativas" className={({ isActive }) => `px-4 py-2 rounded-lg text-sm font-bold uppercase tracking-wide transition-all border ${isActive ? 'bg-white text-green-900 border-white shadow-md' : 'text-white border-transparent hover:bg-white/10'}`}>
                 Unidades
               </NavLink>
-              {/*<NavLink to="/inspeccion/nueva" className={({ isActive }) =>`px-4 py-2 rounded-lg text-sm font-bold uppercase tracking-wide transition-all border ${isActive ? 'bg-white text-green-900 border-white shadow-md' : 'text-white border-transparent hover:bg-white/10'}` }>
+              <NavLink to="/inspeccion/nueva" className={({ isActive }) =>`px-4 py-2 rounded-lg text-sm font-bold uppercase tracking-wide transition-all border ${isActive ? 'bg-white text-green-900 border-white shadow-md' : 'text-white border-transparent hover:bg-white/10'}` }>
                 Nueva Inspección
-              </NavLink>*/} 
+              </NavLink>
             </nav>
 
             {/* BOTÓN HAMBURGUESA MOVIL */}
