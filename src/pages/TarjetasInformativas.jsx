67d00deecfc9da810fe024dad90375b14bce1dca
imssbienestar-oss <irving.hernandezimssb@gmail.com>
2026-02-20 15:58:34 -0600

    Update TarjetasInformativas.jsx

--- src/pages/TarjetasInformativas.jsx
+++ src/pages/TarjetasInformativas.jsx
@@ -506,7 +506,7 @@ function TarjetasInformativas() {
                       </div>
                     )}
 
-                    {/*
+                 
                     {linkInspeccion ? (
                       <a
                         href={linkInspeccion}
@@ -523,7 +523,7 @@ function TarjetasInformativas() {
                       <div className="flex items-center justify-center gap-2 px-4 py-2 rounded-lg font-bold text-amber-600 bg-amber-50 border border-amber-100 text-[10px] uppercase">
                         Insp. Pendiente
                       </div>
-                    )}*/}
+                    )}
                   </div>
                 </div>
               </div>
