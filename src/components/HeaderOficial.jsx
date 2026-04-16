35460b85f95afe436a2aa442fbb421fb2ebbc0ea
imssbienestar-oss <irving.hernandezimssb@gmail.com>
2026-04-16 17:37:27 -0600

    Update HeaderOficial.jsx

--- src/components/HeaderOficial.jsx
+++ src/components/HeaderOficial.jsx
@@ -38,10 +38,6 @@ const HeaderOficial = () => {
 
             {/* MENÚ PC */}
             <nav className="hidden md:flex items-center gap-3">
-              {/* 
-              <NavLink to="/" className={({ isActive }) => `px-4 py-2 rounded-lg text-sm font-bold uppercase tracking-wide transition-all border ${isActive ? 'bg-white text-green-900 border-white shadow-md' : 'text-white border-transparent hover:bg-white/10'}`}>
-                Directorio Personal
-              </NavLink>*/}
              {/* <NavLink to="/estadisticas" className={({ isActive }) => `px-4 py-2 rounded-lg text-sm font-bold uppercase tracking-wide transition-all border ${isActive ? 'bg-white text-green-900 border-white shadow-md' : 'text-white border-transparent hover:bg-white/10'}`}>
                 Estadísticas
               </NavLink>*/} 
