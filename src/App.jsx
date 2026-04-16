e150fbde75b76e36e343079cdd178e574a94b79f
imssbienestar-oss <irving.hernandezimssb@gmail.com>
2026-04-16 17:38:29 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 // Importamos las páginas desde sus nuevas carpetas
-import Directorio from './pages/Directorio';
 import PerfilFuncionario from './pages/PerfilFuncionario';
 import TarjetasInformativas from './pages/TarjetasInformativas';
 import Estadisticas from './pages/Estadisticas';
@@ -13,7 +12,6 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Directorio />} />
         <Route path="/informativas" element={<TarjetasInformativas />} />
         <Route path="/perfil/:slug" element={<PerfilFuncionario />} />
         <Route path="/estadisticas" element={<Estadisticas />} />
